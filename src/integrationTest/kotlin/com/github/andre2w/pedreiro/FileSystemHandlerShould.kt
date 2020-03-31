3cec773c1472504fb6084108475b5b188472bc41
Andre Torres <andre855@gmail.com>
2020-03-31 18:49:46 +0100

    Add sourceSet for Integration Test

---
+++ src/integrationTest/kotlin/com/github/andre2w/pedreiro/FileSystemHandlerShould.kt
@@ -0,0 +1,12 @@
+package com.github.andre2w.pedreiro
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FileSystemHandlerShould {
+
+    @Test
+    fun test() {
+        Assertions.assertTrue(true)
+    }
+}
