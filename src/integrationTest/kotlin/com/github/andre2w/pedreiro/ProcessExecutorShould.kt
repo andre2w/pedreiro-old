a5a44d9a56b226411b894c744ae349be689442ae
andre2w <andre855@gmail.com>
2020-04-05 14:47:26 +0100

    Implement ProcessExecutor and integration tests for it

---
+++ src/integrationTest/kotlin/com/github/andre2w/pedreiro/ProcessExecutorShould.kt
@@ -0,0 +1,24 @@
+package com.github.andre2w.pedreiro
+
+import com.github.andre2w.pedreiro.io.ProcessExecutor
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.Instant
+
+class ProcessExecutorShould {
+
+    @Test
+    fun `execute shell commands in the specified folder`() {
+        val processExecutor = ProcessExecutor()
+        val userDir = System.getProperty("user.dir")
+        val fileName = "test-${Instant.now()}.txt"
+
+        processExecutor.execute("touch $fileName", userDir)
+
+        val createdFile = "$userDir/$fileName"
+        assertThat(Files.exists(Paths.get(createdFile))).isTrue()
+        Files.delete(Paths.get(createdFile))
+    }
+}
--- src/main/kotlin/com/github/andre2w/pedreiro/io/ProcessExecutor.kt
+++ src/main/kotlin/com/github/andre2w/pedreiro/io/ProcessExecutor.kt
@@ -1,8 +1,14 @@
 package com.github.andre2w.pedreiro.io
 
+import java.io.File
+
 class ProcessExecutor {
     fun execute(command: String, runFolder: String) : Int {
-        TODO("Not yet implemented")
-    }
+        val result = ProcessBuilder()
+            .command(command.split(" "))
+            .directory(File(runFolder))
+            .start()
 
+        return result.exitValue()
+    }
 }
