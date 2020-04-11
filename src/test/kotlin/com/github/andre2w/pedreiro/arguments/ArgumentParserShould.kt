9305acd0542936c9cdda2ceeabf71c1941a8bee7
andre2w <andre855@gmail.com>
2020-04-11 14:34:06 +0100

    Add test for multiple extra arguments

--- src/test/kotlin/com/github/andre2w/pedreiro/arguments/ArgumentParserShould.kt
+++ src/test/kotlin/com/github/andre2w/pedreiro/arguments/ArgumentParserShould.kt
@@ -2,6 +2,8 @@ package com.github.andre2w.pedreiro.arguments
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class ArgumentParserShould {
 
@@ -15,19 +17,33 @@ class ArgumentParserShould {
         assertThat(arguments).isEqualTo(Arguments("baseGradle"))
     }
 
-    @Test
-    internal fun `parse -a and --arg to extra arguments`() {
+    @ParameterizedTest
+    @CsvSource(
+        "-a, project_name, pedreiro",
+        "--arg, project_name, pedreiro"
+    )
+    fun `parse -a and --arg to extra arguments`(arg : String, argName: String, argValue: String) {
         val argumentParser = ArgumentParser()
-        val argList = arrayOf(
-            "baseGradle",
-            "-a",
-            "project_name=pedreiro"
+
+        val arguments = argumentParser.parse(arrayOf("baseGradle", arg, "$argName=$argValue"))
+
+
+        val extraArgs = mapOf(argName to argValue)
+        assertThat(arguments).isEqualTo(Arguments("baseGradle", extraArgs))
+    }
+
+
+    @Test
+    internal fun `accumulate extra arguments`() {
+       val argumentParser = ArgumentParser()
+
+        val arguments = argumentParser.parse(
+            arrayOf("baseGradle", "-a","project_name=pedreiro","--arg","project_group=com.github.andre2w")
         )
 
-        val arguments = argumentParser.parse(argList)
-
         val extraArgs = mapOf(
-            "project_name" to "pedreiro"
+            "project_name" to "pedreiro",
+            "project_group" to "com.github.andre2w"
         )
         assertThat(arguments).isEqualTo(Arguments("baseGradle", extraArgs))
     }
